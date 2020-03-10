b06974ea21946c38cea51f47812f3870d2699adc
Andy Marshall <ajmarshall@mail.com>
2020-03-10 23:12:35 +0000

    Add files via upload

---
+++ styles/global.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    padding: 20
+  },
+  box: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 25
+  },
+  splitBox: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
