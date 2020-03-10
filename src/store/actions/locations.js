f1e4fba7c3db7965eddf0161b42189faf9ad5bdf
Andy Marshall <ajmarshall@mail.com>
2020-03-10 21:12:22 +0000

    Add files via upload

--- src/store/actions/locations.js
+++ src/store/actions/locations.js
@@ -1,11 +1,25 @@
-import { DELETE_LOCATION, SELECT_LOCATION, DESELECT_LOCATION, SET_LOCATIONS } from './actionTypes'; 
-import { uiStartLoading, uiStopLoading} from './index';
+import {
+  DELETE_LOCATION,
+  SELECT_LOCATION,
+  DESELECT_LOCATION,
+  SET_LOCATIONS
+} from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./index";
+import * as firebase from "firebase";
+import { snapshotToArray } from "../../components/Extras/Extras";
 
-export const addLocation = (locationName, locationType, coordinates, locationDesc,
-    locationArea, locationRating) => {
-    return dispatch => {
-        dispatch(uiStartLoading());
-        const locationData = {
+export const addLocation = (
+  locationName,
+  selected1,
+  coordinates,
+  locationDesc,
+  locationArea,
+  locationRating,
+  userId
+) => {
+  return dispatch => {
+    dispatch(uiStartLoading());
+    /*const locationData = {
             name: locationName,
             type: locationType,
             coordinates: coordinates,
@@ -17,72 +31,77 @@ export const addLocation = (locationName, locationType, coordinates, locationDes
         fetch("https://tm470-b482f.firebaseio.com/locations.json", {
             method: "POST",
             body: JSON.stringify(locationData)
-        })
-        
-        .then(res => res.json())
-        .then(parsedRes => {
-            console.log(parsedRes);
-            dispatch(uiStopLoading());
-        })
-        .then(alert('Location added'))
-        .catch(err => {
-            console.log(err);
-            alert("Something went wrong, sorry. The location could not be added. Check your internet connection :/");
-           dispatch(uiStopLoading()); 
-        });
-    };
-        
+        })*/
+    firebase
+      .database()
+      .ref("locations/")
+      .push({
+        name: locationName,
+        type: selected1,
+        coordinates: coordinates,
+        description: locationDesc,
+        area: locationArea,
+        ratingNumber: locationRating,
+        totalRatings: 1,
+        totalRatingsStars: locationRating,
+        created_at: Date.now(),
+        userId: userId
+      })
+      .then(() => dispatch(uiStopLoading()))
+      .then(alert("Location added"))
+      .catch(err => {
+        console.log(err);
+        alert(
+          "Something went wrong, sorry. The location could not be added. Check your internet connection :/"
+        );
+        dispatch(uiStopLoading());
+      });
+  };
 };
 
 export const getLocations = () => {
-    return dispatch => {
-        dispatch(uiStartLoading());
-        fetch("https://tm470-b482f.firebaseio.com/locations.json")
-        
-          .then(res => res.json())
-          .then(parsedRes => {
-              const locations = [];
-              for (let key in parsedRes) {
-                  locations.push({
-                      ...parsedRes[key],
-                      key: key
-                  })
-              }
-            dispatch(setLocations(locations));
-            dispatch(uiStopLoading());
-          })
-          .catch(err => {
-            alert("Something went wrong, sorry. The locations could not be fetched. Check your internet connection. :/");
-            dispatch(uiStopLoading());
-            console.log(err);
-        });     
-        };
+  return dispatch => {
+    dispatch(uiStartLoading());
+    firebase
+      .database()
+      .ref("/locations")
+      .once("value", snapshot => {
+        const locations = snapshotToArray(snapshot);
+        dispatch(setLocations(locations));
+        dispatch(uiStopLoading());
+      })
+      .catch(err => {
+        alert(
+          "Something went wrong, sorry. The locations could not be fetched. Check your internet connection. :/"
+        );
+        dispatch(uiStopLoading());
+        console.log(err);
+      });
+  };
 };
 
 export const setLocations = locations => {
-    return {
-        type: SET_LOCATIONS,
-        locations: locations
-    };
+  return {
+    type: SET_LOCATIONS,
+    locations: locations
+  };
 };
-
-
 
 export const deleteLocation = () => {
-    return {
-        type: DELETE_LOCATION
-    };
+  return {
+    type: DELETE_LOCATION
+  };
 };
 
-export const selectLocation = (key) => {
-    return {
-        type: SELECT_LOCATION,
-        locationKey: key
-    };
+export const selectLocation = key => {
+  return {
+    type: SELECT_LOCATION,
+    locationKey: key
+  };
 };
 
 export const deselectLocation = () => {
-    return {
-        type: DESELECT_LOCATION
-    };
+  return {
+    type: DESELECT_LOCATION
+  };
 };
